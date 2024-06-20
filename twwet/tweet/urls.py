9ee9eece6b5105d2ff0320ac56de17362ff640eb
SureshChandraAdhikari <sureshadhikari2002@gmail.com>
2024-06-20 23:41:15 +0530

    crud of tweet

--- twwet/tweet/urls.py
+++ twwet/tweet/urls.py
@@ -1,11 +1,19 @@
 # from django.contrib import admin
 from django.urls import path
-# from django.conf import settings
-# from django.conf.urls.static import static
-
 from . import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('',views.index,name='index')
+    path('', views.tweet_list, name='tweet_list'),
+    path('create/', views.tweet_create, name='tweet_create'),
+    path('<int:tweet_id>/delete/', views.tweet_delete, name='tweet_delete'),
+    path('<int:tweet_id>/edit/', views.tweet_edit, name='tweet_edit'),
 ]
+
+# If you need to serve static and media files in development, uncomment the following lines:
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
