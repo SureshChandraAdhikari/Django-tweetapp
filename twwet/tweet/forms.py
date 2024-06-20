60758cafeffa606ec9fc04e3e158bb9bfad7ca46
SureshChandraAdhikari <sureshadhikari2002@gmail.com>
2024-06-21 00:45:03 +0530

    add user authentication

--- twwet/tweet/forms.py
+++ twwet/tweet/forms.py
@@ -1,8 +1,16 @@
-
+from django.contrib.auth.models import User
 from django import forms
 from .models import Tweet
+from django.contrib.auth.forms import UserCreationForm
 
 class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
         fields = ['text' , 'photo']
+        
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
